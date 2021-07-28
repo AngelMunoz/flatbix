e1357ff630f5b00f2ea655dbe29d4f3a5258a0f5
Angel D. Munoz <scythes.lol@gmail.com>
2021-07-28 00:29:19 -0500

    add flatbixuser

--- Shared/Types.fs
+++ Shared/Types.fs
@@ -1,13 +1,59 @@
 module Shared.Types
 
+open System
+#if FABLE_COMPILER
+open Thoth.Json
+#else
+open Thoth.Json.Net
+#endif
 
-type SigninPayload = { email: string; password: string }
+type FlatbixUser =
+  { id: Guid
+    username: string
+    email: string }
+  static member Decoder: Decoder<FlatbixUser> =
+    Decode.object
+      (fun get ->
+        { id = get.Required.Field "id" Decode.guid
+          username = get.Required.Field "username" Decode.string
+          email = get.Required.Field "email" Decode.string })
+
+  static member Encoder(payload: FlatbixUser) =
+    Encode.object [ "id", Encode.guid payload.id
+                    "username", Encode.string payload.username
+                    "email", Encode.string payload.email ]
+
+type SigninPayload =
+  { email: string
+    password: string }
+
+  static member Decoder: Decoder<SigninPayload> =
+    Decode.object
+      (fun get ->
+        { email = get.Required.Field "email" Decode.string
+          password = get.Required.Field "password" Decode.string })
+
+  static member Encoder(payload: SigninPayload) =
+    Encode.object [ "email", Encode.string payload.email
+                    "password", Encode.string payload.password ]
+
 
 type SignupPayload =
   { email: string
     password: string
     username: string }
 
+  static member Decoder: Decoder<SignupPayload> =
+    Decode.object
+      (fun get ->
+        { email = get.Required.Field "email" Decode.string
+          password = get.Required.Field "password" Decode.string
+          username = get.Required.Field "username" Decode.string })
+
+  static member Encoder(payload: SignupPayload) =
+    Encode.object [ "email", Encode.string payload.email
+                    "password", Encode.string payload.password
+                    "username", Encode.string payload.username ]
 
 [<RequireQualifiedAccess>]
 type Action =
